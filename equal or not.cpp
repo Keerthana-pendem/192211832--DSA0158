3fd05330597efcc4327506ef851b7640b517c7db
Keerthana-pendem <134472936+Keerthana-pendem@users.noreply.github.com>
2024-07-23 08:31:21 +0530

    Add files via upload

---
+++ equal or not.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n1,n2;
+cout<<"enter first number:";
+cin>>n1;
+cout<<"enter second number:";
+cin>>n2;
+if(n1==n2)
+{
+cout<<"the numbers are equal"<<endl;	
+}
+else{
+	cout<<"the numbers are not equal"<<endl;
+}
+return 0;
+}
