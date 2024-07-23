b282b5ee294f57d11ccc3bd44118f3d5aebcdd17
Keerthana-pendem <134472936+Keerthana-pendem@users.noreply.github.com>
2024-07-23 08:32:10 +0530

    Add files via upload

---
+++ average of three numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1, n2, n3;
+	cout<<"enter first number:";
+	cin>>n1;
+	cout<<"enter second number:";
+	cin>>n2;
+	cout<<"enter third number:";
+	cin>>n3;
+	int avg = (n1+n2+n3)/3;
+	
+	cout<<"average of three numbers would be:"<<" "<<avg<<endl;
+	return 0;
+}
