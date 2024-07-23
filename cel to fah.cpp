aa05f3af633fc45d099a2bc1769107793f4e7be2
Keerthana-pendem <134472936+Keerthana-pendem@users.noreply.github.com>
2024-07-23 08:26:11 +0530

    Add files via upload

---
+++ cel to fah.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float cel;
+	float fah;
+	cout<<"Enter celcius:";
+	cin>>cel;
+	fah=(cel*9/5)+32;
+	cout<<"Fahrenheit is="<<fah;
+	return 0;
+}
