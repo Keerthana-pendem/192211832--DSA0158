abfcd5172f4857f7a2f35d548d8beaf51df39bec
Keerthana-pendem <134472936+Keerthana-pendem@users.noreply.github.com>
2024-07-23 08:30:02 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1;
+	cout<<"enter the number:";
+	cin>>n1;
+	if(n1%2==0)
+	{
+		cout<<n1<<" "<<"is a even number"<<endl;
+	}
+	else
+	{
+		cout<<n1<<" "<<"is a odd number"<<endl;
+	}
+	return 0;
+}
