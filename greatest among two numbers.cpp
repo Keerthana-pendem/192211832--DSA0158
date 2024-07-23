a5d197114f5833a141001efee7137947af39e1b0
Keerthana-pendem <134472936+Keerthana-pendem@users.noreply.github.com>
2024-07-23 08:30:23 +0530

    Add files via upload

---
+++ greatest among two numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+	cout<<"enter first number:";
+	cin>>n1;
+	cout<<"enter second number:";
+	cin>>n2;
+	if(n1>n2)
+	{
+		cout<<n1<<" "<<"is a great number"<<endl;
+		}	
+		else
+		{
+			cout<<n2<<" "<<"is a great number"<<endl;
+		}
+		return 0;
+}
+
